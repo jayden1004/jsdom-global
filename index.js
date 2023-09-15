0200626e1dcb8ac128d95b3c4adf6d764a0c2933
JW <78610521+jayden1004@users.noreply.github.com>
2023-09-15 16:33:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,11 @@ module.exports = function globalJsdom (html, options) {
     appName: "Netscape",
     appVersion: "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
     cookieEnabled: true,
+    product: "Gecko",
     language: "en-GB",
     onLine: true,
+    webdriver: false,
+    languages: ["en-GB", "en-US", "en"],
     platform: "Win32",
     userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
   });
