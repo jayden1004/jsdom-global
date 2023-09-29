b3c3d7f4ada0dbd32220a11c6f3a3d05da8eb53f
JW <78610521+jayden1004@users.noreply.github.com>
2023-09-29 13:28:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ module.exports = function globalJsdom (html, options) {
     webdriver: false,
     languages: ["en-GB", "en-US", "en"],
     platform: "Win32",
-    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
   });
   options.resources = resourceLoader;
   var document = new jsdom.JSDOM(html, options)
